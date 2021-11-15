bf82ab38d04fdbc44a2797e8123eb417e61aac0d
Alex Viapiano <alexviapiano@hotmail.com>
2021-11-14 22:11:59 -0500

    Rebuidl

--- extensions/users-permissions/models/User.js
+++ extensions/users-permissions/models/User.js
@@ -3,7 +3,7 @@ module.exports = {
     async beforeCreate(data) {},
     async afterCreate(result, data) {
       if (result.creator) {
-        console.log("Set user " + result.id + " to creator role");
+        console.log("Set user " + result.id + " to creator role!");
         strapi
           .query("user", "users-permissions")
           .update({ id: result.id }, { role: 3 });
