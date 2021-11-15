c0ee0bde41a959c4c157842b5f2e067911a1d326
Alex Viapiano <alexviapiano@hotmail.com>
2021-11-14 21:40:10 -0500

    Added fields to user & new role creator & auto assign creator role when user.creator == true

---
+++ extensions/users-permissions/models/User.js
@@ -0,0 +1,13 @@
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {},
+    async afterCreate(result, data) {
+      if (result.creator) {
+        console.log("Set user " + result.id + " to creator role");
+        strapi
+          .query("user", "users-permissions")
+          .update({ id: result.id }, { role: 3 });
+      }
+    },
+  },
+};
