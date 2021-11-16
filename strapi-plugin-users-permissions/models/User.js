dbf89359ccc5a5a0b72a8d2d7de6b670f1235f30
Alex Viapiano <alexviapiano@hotmail.com>
2021-11-16 12:49:38 -0500

    Add user lifestyle method

--- strapi-plugin-users-permissions/models/User.js
+++ strapi-plugin-users-permissions/models/User.js
@@ -1,7 +1,19 @@
-'use strict';
+"use strict";
 
 /**
  * Lifecycle callbacks for the `User` model.
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {},
+    async afterCreate(result, data) {
+      if (result.creator) {
+        console.log("Set user " + result.id + " to creator role!");
+        strapi
+          .query("user", "users-permissions")
+          .update({ id: result.id }, { role: 3 });
+      }
+    },
+  },
+};
